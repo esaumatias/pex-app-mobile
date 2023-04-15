c19b30b39eb934b9ecdda14ce1dd9163835f656a
esau <esaufreirematias@gmail.com>
2023-04-15 14:43:18 -0300

    feat(Home) - implementa carousel com cards de produtos

---
+++ screens/Home/components/CarouselCartItem.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { View, Dimensions, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+
+const { width, height } = Dimensions.get('window');
+
+import product1 from '../../../assets/product1.jpg';
+import product2 from '../../../assets/product2.jpg';
+
+const data = [
+  { id: 1, image: product1, title: 'Torneira Puravida Hansgrohe', subtitle: 'Louças e metais' },
+  { id: 2, image: product2, title: 'Portobello Mont blanc Natural 90x...', subtitle: 'Pisos e porcelanato' },
+  { id: 3, image: product1, title: 'Torneira Puravida Hansgrohe', subtitle: 'Louças e metais' },
+  { id: 4, image: product2, title: 'Portobello Mont blanc Natural 90x...', subtitle: 'Pisos e porcelanato' },
+];
+
+const CarouselItem = ({ item }) => {
+  return (
+    <View style={styles.carouselItem}>
+      <Image source={item.image} style={styles.carouselImage} />
+      <View style={styles.carouselContent}>
+        <Text style={styles.carouselSubtitle}>{item.subtitle}</Text>
+        <Text style={styles.carouselTitle}>{item.title}</Text>
+        <TouchableOpacity style={styles.carouselButton}>
+          <Text style={styles.carouselButtonText}>Comprar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const CarouselCartItem = () => {
+  const carouselRef = React.useRef(null);
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const handleSnapToItem = (index) => {
+    setCurrentIndex(index);
+  };
+
+  const handleCarouselItemPress = (index) => {
+    carouselRef.current.snapToItem(index);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Carousel
+        ref={carouselRef}
+        data={data}
+        renderItem={CarouselItem}
+        sliderWidth={width}
+        itemWidth={150}
+        sliderHeight={height}
+        itemHeight={height - 120}
+        onSnapToItem={handleSnapToItem}
+        loop={true}
+        autoplay={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    marginTop: 20,
+  },
+  carouselItem: {
+    borderWidth: 1.5,
+    borderColor: '#F1F0F3',
+    borderRadius: 12,
+    width: 141,
+    height: 223,
+    alignItems: 'center',
+    paddingVertical: 5,
+  },
+  carouselImage: {
+    width: 125,
+    height: 108,
+    borderRadius: 8
+  },
+  carouselTextContainer: {
+    position: 'absolute',
+    bottom: 0,
+    marginBottom: 80,
+    marginLeft: 25,
+  },
+  carouselTitle: {
+    color: '#000',
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  carouselSubtitle: {
+    color: '#999999',
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 8,
+    lineHeight: 10,
+    marginTop: 5
+  },
+  carouselContent: {
+    paddingHorizontal: 8,
+  },
+  carouselButtonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    marginBottom: 20,
+    right: 20,
+    zIndex: 2,
+  },
+  carouselButton: {
+    backgroundColor: '#F09200',
+    borderRadius: 12,
+    height: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  carouselButtonText: {
+    color: '#FFFFFF',
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 8,
+    lineHeight: 10,
+  },
+});
+
+export default CarouselCartItem;
--- screens/Home/index.js
+++ screens/Home/index.js
@@ -11,6 +11,7 @@ import {
 import { TopBar } from "../../components/TopBar";
 import { SessionContext } from "../../providers/SessionProvider";
 import MyCarousel from '../../components/MyCarousel';
+import CarouselCartItem from './components/CarouselCartItem';
 
 import logoHome from "../../assets/logoHome.png";
 import sinoIcon from "../../assets/sinoIcon.png";
@@ -30,15 +31,35 @@ export const Home = ({ navigation, route }) => {
           <View style={styles.containerIconTop}>
             <Image source={logoHome} />
 
-            <View style={{ flexDirection: 'row', gap: 20}}>
+            <View style={{ flexDirection: "row", gap: 20 }}>
               <Image source={cartIcon} />
               <Image source={sinoIcon} />
             </View>
-
           </View>
         </View>
 
         <MyCarousel />
+
+        <Text style={{...styles.texto, marginTop: 20}}>Categorias</Text>
+
+        <View style={styles.containerButons}>
+          <View
+            style={styles.buttonCadatrar}
+          >
+            <Text style={{ ...styles.texto, color: "#FFFFFF" }}>Todas</Text>
+          </View>
+
+          <View style={styles.buttonPular}>
+            <Text style={styles.texto}>Ofertas</Text>
+          </View>
+
+          <View style={styles.buttonPular}>
+            <Text style={styles.texto}>Pisos e porcelanatos</Text>
+          </View>
+        </View>
+
+        <Text style={{...styles.texto, textAlign: 'left', paddingHorizontal: 32}}>Produtos populares</Text>
+        <CarouselCartItem />
       </ScrollView>
     </SafeAreaView>
   );
@@ -55,5 +76,44 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
-  }
+  },
+  texto: {
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 14,
+    lineHeight: 18,
+    display: "flex",
+    alignItems: "center",
+    textAlign: "center",
+    color: "#000",
+  },
+  containerButons: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 32,
+    marginBottom: 30,
+    // gap: 6,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 15,
+  },
+  buttonCadatrar: {
+    paddingHorizontal: 10,
+    height: 32,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#7B61FF",
+    borderRadius: 52,
+  },
+  buttonPular: {
+    paddingHorizontal: 10,
+    height: 32,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1.5, 
+    borderColor: '#F1F0F3', 
+    borderRadius: 52,
+  },
 });
